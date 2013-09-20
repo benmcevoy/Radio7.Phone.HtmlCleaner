307ed6138dbafe1660de5caabd14a893f2856b15
benmcevoy <ben.mcevoy@gmail.com>
2013-09-20 14:16:01 +1000

    initial

---
+++ Radio7.Phone.HtmlCleaner/Extractors/Content/IContentExtractor.cs
@@ -0,0 +1,10 @@
+﻿using Radio7.Phone.HtmlCleaner.Entities;
+using System;
+
+namespace Radio7.Phone.HtmlCleaner.Extractors.Content
+{
+    public interface IContentExtractor
+    {
+        ExtractedContent Extract(string html, Uri documentUrl);
+    }
+}
