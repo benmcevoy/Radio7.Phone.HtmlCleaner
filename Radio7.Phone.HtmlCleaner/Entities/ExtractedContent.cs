b2222d11e439cab2edf35217f7849947bb9751fd
benmcevoy <ben.mcevoy@gmail.com>
2013-09-20 15:46:41 +1000

    removed summary as it is not a responsibilty here

--- Radio7.Phone.HtmlCleaner/Entities/ExtractedContent.cs
+++ Radio7.Phone.HtmlCleaner/Entities/ExtractedContent.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Radio7.Phone.HtmlCleaner.Entities
 {
@@ -10,10 +9,6 @@ namespace Radio7.Phone.HtmlCleaner.Entities
         public string Title  { get; set; }
 
         public string Text { get; set; }
-
-        public string Summary { get; set; }
-
-        public IEnumerable<string> Keywords { get; set; }
 
         public string Html { get; set; }
 
